6b7a2ed5d9dfb5ba5645b4ace9ba3bc25a3f9b04
huynth1801 <huuhuy1801@gmail.com>
2021-11-04 11:26:24 +0700

    update model.py

--- train.py
+++ train.py
@@ -121,8 +121,8 @@ def train_model(net, dataloader_dict, criterion, optimizer, writer, num_epochs):
                         epoch_train_loss += loss.item()
                         iteration += 1
                     else:
-                        epoch_val_loss += loss.item()
                         writer.add_scalar('Val Loss/Epoch', loss, epoch)
+                        epoch_val_loss += loss.item()
         t_epoch_end = time.time()
         print("---"*20)
         print("Epoch {} || Epoch_train_loss: {:.4f} || Epoch_val_loss: {:.4f}".format(epoch+1, epoch_train_loss,
